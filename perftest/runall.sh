c6b98a0a6abde9825c6884210bced32e1e909a2c
Thangakrishnan <thangakr@amazon.com>
2016-11-14 12:03:52 -0800

    Convenience script to run all three models.

---
+++ perftest/runall.sh
@@ -0,0 +1,3 @@
+bash runscalabilitytest.sh -h nodes -m resnet -g 16 -b 32 -r /tmp/ -x 128
+bash runscalabilitytest.sh -h nodes -m inceptionv3 -g 16 -b 32 -r /tmp/ -x 128
+bash runscalabilitytest.sh -h nodes -m alexnet -g 16 -b 512 -r /tmp/ -x 128
